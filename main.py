03577f7a98115683ce3bba3f528f5c5d81b667ca
torineris <vitorianerisrebelo@gmail.com>
2023-02-01 10:18:31 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pydicom
 
+def recurse(dataset):
+    for block in dataset:
+        if block.tag == (0x0040, 0xa730):
+            for item in block:
+                code_meaning = item['ConceptNameCodeSequence'][0]['CodeMeaning'].value
+
+                try:
+                    numeric_value = item['MeasuredValueSequence'][0]['NumericValue'].value
+                except:
+                    numeric_value = 'Indefinido'
+
+                generating_string = str(code_meaning) + ': ' + str(numeric_value) + '\n'
+
+                file = open('structured_report.txt', 'a')
+                file.write(generating_string)
+                file.close()
+
+                [recurse(item) for item in block.value]
+
 sr = pydicom.dcmread('sr.dcm')
 
-for elem in sr['ContentSequence']:
-    try:
-        for elem in elem['ContentSequence']:
-            print(elem['ConceptNameCodeSequence'][0]['CodeMeaning'].value)
-            try:
-                print(elem['MeasuredValueSequence'][0])
-            except:
-                print('INDEFINIDO;')
-    except:
-        print('Aguardando o próximo')
-        
+recurse(sr)
