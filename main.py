8e0266a99d6c5b8e3281fa2bfe45a3c342d80411
Dymas <dymassousa@gmail.com>
2023-02-03 10:58:55 -0300

    Adicionando pesquisa por nome da medida

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pydicom
 
-def recurse(dataset):
-    parsed_structured_report = {}
+sr = pydicom.dcmread('sr.dcm')
+index = 0
 
+def recurse(dataset):
     for block in dataset:
         if block.tag == (0x0040, 0xa730):
+            global index
             for item in block:
                 code_meaning = item['ConceptNameCodeSequence'][0]['CodeMeaning'].value
 
@@ -13,16 +15,39 @@ def recurse(dataset):
                 except:
                     numeric_value = 'Indefinido'
 
-                parsed_structured_report[code_meaning] = numeric_value
+                parsed_structured_reporting.update({
+                    index: {
+                        'measure_name': code_meaning,
+                        'value': numeric_value
+                    }
+                })
+
+                index += 1
 
                 [recurse(item) for item in block.value]
     
-    file = open('structured_report.txt', 'a')
-    file.write(str(parsed_structured_report))
-    file.close()
+    return parsed_structured_reporting
 
-    return parsed_structured_report
+def searchMeasureName(parsed_structured_reporting, measure_name):
+    found_results = {}
+    index = 0
 
-sr = pydicom.dcmread('sr.dcm')
+    for item in parsed_structured_reporting:
+        if parsed_structured_reporting[item]['measure_name'] == measure_name:
+            found_results[index] = parsed_structured_reporting[item]
+            index += 1
 
-recurse(sr)
+    return found_results
+
+parsed_structured_reporting = {}
+reporting_parsed = recurse(sr)
+
+file = open('structured_reporting.txt', 'a')
+file.write(str(reporting_parsed))
+file.close()
+
+found_results = searchMeasureName(reporting_parsed, 'Femur Length')
+
+file = open('search_found_results.txt', 'a')
+file.write(str(found_results))
+file.close()
