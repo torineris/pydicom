7dfb8a29727340e1f40219807cf21013f8745ff6
torineris <vitorianerisrebelo@gmail.com>
2023-02-10 11:08:20 -0300

    Remoção de funções

--- reportgenerator.py
+++ reportgenerator.py
@@ -23,36 +23,3 @@ for item in tags:
             data.update({name:result})
         return data
     dataCollector(item)
-
-
-'''#Cálculo da média da idade gestacional
-agelist=[]
-for key, value in gestational_age.items():
-    agelist.append(value['value'])
-
-def medval(list):
-    media = sum(list)/len(list)
-    return media'''
-
-
-#Função de criação do PDF
-def GeneratePDF(data):
-    try:
-        nome_pdf = 'Teste'
-        pdf = canvas.Canvas('{}.pdf'.format(nome_pdf))
-        x = 720
-        for key,value in data.items():
-            x -= 20
-            pdf.drawString(247,x, '{} : {}'.format(key,value))
-        pdf.setTitle(nome_pdf)
-        pdf.setFont("Helvetica-Oblique", 14)
-        pdf.drawString(245,750, 'Lista de tags')
-        pdf.setFont("Helvetica-Bold", 12)
-        pdf.drawString(245,724, 'Nome da medida e valor')
-        pdf.save()
-        print('{}.pdf criado com sucesso!'.format(nome_pdf))
-    except:
-        print('Erro ao gerar {}.pdf'.format(nome_pdf))
-
-
-GeneratePDF(data)
