1be648ad5ef58d358e27ffc50b6fe564c47c1268
torineris <vitorianerisrebelo@gmail.com>
2023-02-13 15:43:05 -0300

    Removendo funções desnecessárias

--- reportgenerator.py
+++ reportgenerator.py
@@ -1,41 +1,33 @@
 import pydicom
 from structured_reporting_parse.StructuredReportingParse import StructuredReportingParse
-from statistics import median
-from decimal import *
+from statistics import *
 
 structured_reporting_dataset = pydicom.dcmread('data/sr.dcm')
-parsed_sr_instance = StructuredReportingParse(structured_reporting_dataset)
+parsed_sr_instance = StructuredReportingParse(structured_reporting_dataset)      
 
-
-#Função de calculo de média
-def average(gestational_age):
-    return median(gestational_age)
-
-
-#Função para coletar o maior valor de uma lista
-def maximum(hc):
-    return max(hc)
-
-        
 
 #Coletando as medidas
 fetal_biometry=['Gestational Age','Head Circumference','Abdominal Circumference',
 'Amniotic Fluid Index','Biparietal Diameter','Estimated Weight','Femur Length',
 'Fetal Heart Rate','Number of Fetuses','Occipital-Frontal Diameter']
+
+#Armazenando dicionários de medidas
 tags=[]
-gestational_age=[]
-hc=[]
+average_value=[]
+head_c=[]
+
 for item in fetal_biometry:
     tags.append(parsed_sr_instance.searchMeasureName(measure_name=item))
     for elem in tags:
         for key, value in elem.items():
             if value['measure_name'] == 'Gestational Age':
-                gestational_age.append(float(value['value']))
+                average_value.append(value['value'])
             elif value['measure_name'] == 'Head Circumference':
-                hc.append(float(value['value']))
+                head_c.append(value['value'])
 
-g_age = average(gestational_age)
-max_value = maximum(hc)
+#Atribuição de valores tratados
+average_age = mean(average_value)
+max_value = max(head_c)
 
 
 
@@ -45,9 +37,9 @@ for item in tags:
     def dataCollector(item):
         for key, value in item.items():
             if value['measure_name'] == 'Gestational Age':
-                data['Gestational Age']=g_age
+                data['Gestational Age'] = "{:.2f}".format(average_age)
             elif value['measure_name'] == 'Head Circumference':
-                data['Head Circumference']=max_value
+                data['Head Circumference'] = max_value
             else:
                 name = value['measure_name']
                 result = value['value']
