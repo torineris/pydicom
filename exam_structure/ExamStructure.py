c488af91c330cccb18943072b502654477c6a639
torineris <vitorianerisrebelo@gmail.com>
2023-02-17 16:48:04 -0300

    Adicionando método de geração de PDF

--- exam_structure/ExamStructure.py
+++ exam_structure/ExamStructure.py
@@ -1,3 +1,5 @@
+from reportlab.pdfgen import canvas
+
 class ExamStructure:
     def __init__(self, dataset):
         self.dataset = dataset
@@ -25,6 +27,23 @@ class ExamStructure:
                     data_translated.update({gestational_translate[i][1]:value})
                 i+=1
         return data_translated
+    
+    translated_data = translator()
 
-    def createPDF(self):
-        return 'PDF gerado'
+    def createPDF(self, translated_data):
+        try:
+            nome_pdf = 'Fetal Biometry'
+            pdf = canvas.Canvas('{}.pdf'.format(nome_pdf))
+            x = 720
+            for key,value in translated_data.items():
+                x -= 20
+                pdf.drawString(80,x, '{}: {}'.format(key,value))
+            pdf.setTitle(nome_pdf)
+            pdf.setFont("Helvetica-Bold", 14)
+            pdf.drawString(245,750, 'Informações do SR')
+            pdf.setFont("Helvetica", 12)
+            pdf.drawString(245,724, 'Nome da medida e valor')
+            pdf.save()
+            print('{}.pdf criado com sucesso!'.format(nome_pdf))
+        except:
+            print('Erro ao gerar {}.pdf'.format(nome_pdf))
