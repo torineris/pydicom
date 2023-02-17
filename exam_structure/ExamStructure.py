1527032e63d18bb93c2bfc825eafbad1ae180812
torineris <vitorianerisrebelo@gmail.com>
2023-02-17 16:04:41 -0300

    Ajustando método de tradução

--- exam_structure/ExamStructure.py
+++ exam_structure/ExamStructure.py
@@ -1,23 +1,24 @@
-gestational_translate = (
-    ('Abdominal Circumference', 'Circunferência Abdominal'),
-    ('Amniotic Fluid Index', 'Índice de Líquido Amniótico'),
-    ('Biparietal Diameter', 'Diâmetro Biparietal'),
-    ('Estimated Weight', 'Peso Estimado'),
-    ('Femur Length', 'Comprimento do Fêmur'),
-    ('Fetal Heart Rate', 'Batimentos Cárdio-Fetais Rítmicos'),
-    ('Gestational Age', 'Idade Gestacional'),
-    ('Head Circumference', 'Circunferência Cefálica'),
-    ('Number of Fetuses', 'Número de Fetos'),
-    ('Occipital-Frontal Diameter', 'Diâmetro Occipto-Frontal')     
-)
-
 class ExamStructure:
     def __init__(self, dataset):
         self.dataset = dataset
     
-    def translator(self, dataset):
+    def translator(self):
+
+        gestational_translate = (
+            ('Abdominal Circumference', 'Circunferência Abdominal'),
+            ('Amniotic Fluid Index', 'Índice de Líquido Amniótico'),
+            ('Biparietal Diameter', 'Diâmetro Biparietal'),
+            ('Estimated Weight', 'Peso Estimado'),
+            ('Femur Length', 'Comprimento do Fêmur'),
+            ('Fetal Heart Rate', 'Batimentos Cárdio-Fetais Rítmicos'),
+            ('Gestational Age', 'Idade Gestacional'),
+            ('Head Circumference', 'Circunferência Cefálica'),
+            ('Number of Fetuses', 'Número de Fetos'),
+            ('Occipital-Frontal Diameter', 'Diâmetro Occipto-Frontal')     
+        )
+
         data_translated={}
-        for key, value in dataset.items():
+        for key, value in self.dataset.items():
             i=0
             for item in gestational_translate:
                 if key == gestational_translate[i][0]:
